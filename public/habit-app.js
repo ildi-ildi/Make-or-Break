151bebe480701aa179b43b56a2c6a428a960d510
ildi-ildi <ildikoszabo@yahoo.com>
2017-03-12 15:15:29 +0000

    delete feature added

--- public/habit-app.js
+++ public/habit-app.js
@@ -4,21 +4,41 @@
 
 
 $(document).ready(function() {
-    getHabit();
+    getHabit(); // loads everything
 
-    $('form').submit(function(event){
+    $('form').submit(function (event) {
         event.preventDefault();
-        $.post('/api/habit',function(){
+        $.post('/api/habit', {
+            name: $('#name').val(), // gets the value from the id
+            description: $('#desc').val(),
+            colour: $('#colour').val(),
+        }, function () {
             getHabit();
         })
     })
+
+
 });
 
 function getHabit(){
     $.getJSON( "/api/habit", function( data ) {
         $('#habitfill').html('');
         $.each(data.habits, function(idx, habit){
-            $('#habitfill').append('<tr><td>' + habit.name + '</td><td>' + habit.description+ '</td><td>' + habit.colour + '</td></tr>');
+            $('#habitfill').append('<tr data-habit="'+habit._id+'"><td>' + habit.name + '</td><td>' + habit.description+ '</td><td>' + habit.colour + '</td><td><button>Delete</button></td></tr>');
+            //data-habit="'+habit._id+  -- gets the id of the row
         });
+        $('button').off('click').on('click', deleteHabit); //off click -preventing the button to be pressed // on click, call the function deleteHabit
     });
-};
+}
+function deleteHabit(event) {
+        event.preventDefault();
+        var id = $(event.target).parent().parent().attr('data-habit'); // event.target - where we clicked on // parent(). to go to <tr> //parent(). to go to <td> // .attr - geting the info from data-habit
+        $.ajax({ // same like post and get - add more functions
+            method: "DELETE",
+            url: "/api/habit",
+            data: { id: id }
+        })
+            .done(function( msg ) { //auto update table when row is deleted
+                getHabit();
+            });
+}
--- server.js
+++ server.js
@@ -4,28 +4,40 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
+
 
 var Habit = require('./app/api/habit.js');
 
 app.use(express.static(path.join(__dirname,"public")));
+app.use(bodyParser());
 
 // viewed at http://localhost:8080
 app.get('/api/habit', function(req, res) { // app.get - reading
-    console.log("hello")
+
     Habit.find({}, function(err,habits) {
         if(err)
             throw err;
-        console.log(err, habits);
         res.status(200);
         res.json( { habits: habits }); // in format json
     });
 });
 
 app.post('/api/habit', function(req,res) {
-    Habit.create({name: "test", description: "short desc of test", colour: "blue"},function(err,habits){
+    console.log(req.body);
+    Habit.create({name: req.body.name,
+                description: req.body.description,
+                colour: req.body.colour },function(err,habits){
         if(err) throw err;
+        res.send();
+    });
+});
+
+app.delete('/api/habit',function(req,res){
+    Habit.findOneAndRemove({_id: req.body.id },function(err,habits){
+                                                        if(err) throw err;
         res.send();
     });
 });
